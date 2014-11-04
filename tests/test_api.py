dd8be0790371462a4b56315a1a51be8587778236
Brooke M. Fujita <buruzaemon@users.noreply.github.com>
2014-11-04 06:01:51 +0000

    test_api.py edited online with Bitbucket

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,10 +6,19 @@ import unittest
 import natto.api as api
 from io import StringIO
 
+
+
 class TestApi(unittest.TestCase):
 
     def setUp(self):
         self.nm = api.MeCab()
+        
+#        if sys.version < '3':
+#            self._u = lambda(s): return(s)
+#            self._b = lambda s: return(s)
+#        else:
+#            self._u = lambda s: return(s.decode(self.nm.__enc))
+#            self._b = lambda s: return(s.encode(self.nm.__enc))
 
     def test_init_unknownoption(self):
         # MeCabError if unrecognized option passed in
@@ -18,43 +27,40 @@ class TestApi(unittest.TestCase):
 
         self.assertIsNotNone(re.search('--unknown', str(ctx.exception)))
 
-#    def test_init_libunset(self):
-#        # load error when MeCab lib is not found
-#        try:
-#            orig_env = os.getenv(api.MeCab.MECAB_PATH)
-#            os.environ[api.MeCab.MECAB_PATH] = "/foo/bar"
-#
-#            with self.assertRaises(api.MeCabError) as cm:
-#                api.MeCab()
-#
-#            self.assertIsNotNone(re.search('cannot load library /foo/bar',
-#                                           str(cm.exception)))
-#        finally:
-#            os.environ[api.MeCab.MECAB_PATH] = orig_env
-#
-#    def test_version(self):
-#        self.assertEqual(self.nm.version, '0.996')
-#
-#    def test_sysdic(self):
-#        sysdic = self.nm.dicts[0]
-#
-#        self.assertIn(sysdic.charset.lower(), ['utf-16',
-#                                               'utf-8',
-#                                               'shift-jis',
-#                                               'euc-jp'])
-#        self.assertRegexpMatches(sysdic.filename, 'sys.dic$')
-#        self.assertEqual(sysdic.type, 0)
-#        self.assertEqual(sysdic.version, 102)
-#
+    def test_init_libunset(self):
+        # load error when MeCab lib is not found
+        try:
+            orig_env = os.getenv(api.MeCab.MECAB_PATH)
+            os.environ[api.MeCab.MECAB_PATH] = "/foo/bar"
+
+            with self.assertRaises(api.MeCabError) as cm:
+                api.MeCab()
+
+            self.assertIsNotNone(re.search('cannot load library /foo/bar',
+                                           str(cm.exception)))
+        finally:
+            os.environ[api.MeCab.MECAB_PATH] = orig_env
+
+    def test_version(self):
+        self.assertEqual(self.nm.version, '0.996')
+
+    def test_sysdic(self):
+        sysdic = self.nm.dicts[0]
+
+        cs = sysdic.charset.lower()
+        self.assertIn(cs, ['utf-16', 'utf-8', 'utf8', 'shift-jis', 'euc-jp'])
+        self.assertIsNotNone(re.search('sys.dic$', sysdic.filename.decode()))
+        self.assertEqual(sysdic.type, 0)
+        self.assertEqual(sysdic.version, 102)
+
 #    def test_parse(self):
 #        morphs = ['日本語', 'だ', 'よ', '、', 'これ', 'が', '。', 'EOS']
-#        utxt = "".join(morphs[0:-1]).decode('utf-8')
-#        res = self.nm.parse(utxt)
+#        txt = "".join(morphs[0:-1])
+#        res = self.nm.parse(txt)
 #        
 #        lines = res.split("\n")
 #        for i, l in enumerate(lines):
-#            print(l)
-#            print(morphs[i].decode('utf-8'))
-#            print
-#            self.assertIsNotNone(re.search(morphs[i].decode('utf-8'), l))
-#            
+#            print morphs[i]
+#            print l
+#            self.assertIsNotNone(re.search(morphs[i], l))
+            
