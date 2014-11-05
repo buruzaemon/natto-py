9819c847408a8e7fdf02ea1fa4e0bc3c77fbf68b
Brooke M. Fujita <buruzaemon@users.noreply.github.com>
2014-11-05 09:18:28 +0000

    environment.py edited online with Bitbucket; 2/3 string support, wip.

--- natto/environment.py
+++ natto/environment.py
@@ -1,33 +1,65 @@
 # -*- coding: utf-8 -*-
+"""Convenience API to obtain information on MeCab environment."""
 import os
 import sys
+
 from subprocess import Popen, PIPE
 
 class MeCabEnv(object):
+    """Convenience class of object to obtain information on MeCab environment.
+
+    This will attempt to obtain the character encoding (charset) of MeCab's
+    system dictionary, which will determine the encoding used when passing
+    strings in and obtaining string results from MeCab.
+
+    Also attempts to locate and obtain the full path to the MeCab library.
+
+    This makes invocations to the mecab and mecab-config (not available on
+    Windows) executables.
+
+    Will defer to the user-provided values in environment variables
+    MECAB_PATH and MECAB_CHARSET.
+
+    """
 
     MECAB_PATH = 'MECAB_PATH'
     MECAB_CHARSET = 'MECAB_CHARSET'
 
     _WINLIB_EXT = "dll"
     _MACLIB_EXT = "dylib"
-    _UNIXLIB_EXT = "so"
 
-    _INFO_SJIS_DEFAULT = "INFO: defaulting MeCab charset to shift-jis"
+    _UNIXLIB_EXT = "so"
     _INFO_EUCJP_DEFAULT = "INFO: defaulting MeCab charset to euc-jp"
+    _INFO_SJIS_DEFAULT = "INFO: defaulting MeCab charset to shift-jis"
+    _INFO_UTF8_DEFAULT = "INFO: defaulting MeCab charset to utf-8"
     _ERROR_NODIC = "ERROR: MeCab dictionary charset not found"
     _ERROR_NOCMD = "ERROR: mecab -D command not recognized"
     _ERROR_NOLIB = "ERROR: %s could not be found, please use MECAB_PATH"
+    _ERROR_MECABD = "ERROR: mecab -D could not be used to locate %s"
+    _ERROR_MECABCONFIG = "ERROR: mecab-config could not locate %s"
 
     def __init__(self):
+        """Initializes the MeCabEnv instance.
+
+        Raises:
+            EnvironmentError: A problem in obtaining the system dictionary info
+                was encountered.
+        """
         self.charset = self.__get_charset()
         self.libpath = self.__get_libpath()
 
     def __get_charset(self):
-        """Return the charset (character encoding) used internally by MeCab.
-        #try MECAB_CHARSET first
-        # try mecab -D next
+        """Return the character encoding (charset) used internally by MeCab.
 
-        # default to euc-jp
+        Charset is that of the system dictionary used by MeCab. Will defer to
+        the user-specified MECAB_CHARSET environment variable, if set.
+
+        Defaults to shift-jis on Windows.
+        Defaults to utf-8 on Mac OS.
+        Defaults to euc-jp, as per MeCab documentation, when all else fails.
+
+        Returns:
+            The full character encoding (charset) used by MeCab.
         """
         cset = os.getenv(self.MECAB_CHARSET)
         if cset:
@@ -35,8 +67,9 @@ class MeCabEnv(object):
         else:
             try:
                 res = Popen(['mecab', '-D'], stdout=PIPE).communicate()
-                if not res[0].startswith('unrecognized'):
-                    dicinfo = res[0].split(os.linesep)
+                lines = res[0].decode()
+                if not lines.startswith('unrecognized'):
+                    dicinfo = lines.split(os.linesep)
                     t = [t for t in dicinfo if t.startswith('charset')]
                     if len(t) > 0:
                         return t[0].split()[1].lower()
@@ -46,15 +79,36 @@ class MeCabEnv(object):
                 else:
                     sys.stderr.write("%s\n" % self._ERROR_NOCMD)
                     raise EnvironmentError(self._ERROR_NOCMD)
-            except:
+            except OSError:
                 if sys.platform == 'win32':
                     sys.stderr.write("%s\n" % self._INFO_SJIS_DEFAULT)
                     return 'shift-jis'
+                elif sys.platform == 'darwin':
+                    sys.stderr.write("%s\n" % self._INFO_UTF8_DEFAULT)
+                    return 'utf-8'
                 else:
                     sys.stderr.write("%s\n" % self._INFO_EUCJP_DEFAULT)
                     return 'euc-jp'
 
     def __get_libpath(self):
+        """Return the full path to the MeCab library.
+
+        On Windows, the path to the system dictionary is used to deduce the
+        path to libmecab.dll.
+
+        Otherwise, mecab-config is used find the libmecab shared object or
+        dynamic library (*NIX or Mac OS, respectively).
+
+        Will defer to the user-specified MECAB_PATH environment variable, if
+        set.
+
+        Returns:
+            The full path to the MeCab library.
+
+        Raises:
+            EnvironmentError: A problem was encountered in trying to locate the
+                MeCab library.
+        """
         libp = os.getenv(self.MECAB_PATH)
         if libp:
             return libp
@@ -65,17 +119,19 @@ class MeCabEnv(object):
                 try:
                     cmd = ['mecab', '-D']
                     res = Popen(cmd, stdout=PIPE).communicate()
-                    if not res[0].startswith('unrecognized'):
-                        dicinfo = res[0].split(os.linesep)
+                    lines = res[0].decode()
+                    if not lines.startswith('unrecognized'):
+                        dicinfo = lines.split(os.linesep)
                         t = [t for t in dicinfo if t.startswith('filename')]
                         if len(t) > 0:
                             ldir = t[0].split('etc')[0][10:].strip()
                             libp = os.path.join(ldir, 'bin', lib)
                         else:
-                            raise EnvironmentError("mecab -D could not be used to locate %s" % lib)
+                            raise EnvironmentError(self._ERROR_MECABD % lib)
                     else:
-                        raise EnvironmentError("Error invoking mecab")
-                except:
+                        raise EnvironmentError(self._ERROR_MECABD % lib)
+                except EnvironmentError:
+                    sys.stderr.write("%s\n" % sys.exc_info()[0])
                     raise EnvironmentError(self._ERROR_NOLIB % lib)
             else:
                 # UNIX-y OS?
@@ -87,12 +143,14 @@ class MeCabEnv(object):
                 try:
                     cmd = ['mecab-config', '--libs-only-L']
                     res = Popen(cmd, stdout=PIPE).communicate()
-                    if not res[0].startswith('unrecognized'):
-                        linfo = res[0].strip()
+                    lines = res[0].decode()
+                    if not lines.startswith('unrecognized'):
+                        linfo = lines.strip()
                         libp = os.path.join(linfo, lib)
                     else:
-                        raise EnvironmentError("mecab-config could not locate %s" % lib)
-                except:
+                        raise EnvironmentError(self._ERROR_MECABCONFIG % lib)
+                except EnvironmentError:
+                    sys.stderr.write("%s\n" % sys.exc_info()[0])
                     raise EnvironmentError(self._ERROR_NOLIB % lib)
 
             if libp and os.path.exists(libp):
