67ac61cd3e15f157513e3ac07125208cb7944bcc
Brooke M. Fujita <buruzaemon@users.noreply.github.com>
2014-11-06 08:53:52 +0000

    test_suite.py edited online with Bitbucket

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
+'''Test suite for natto-py'''
 import unittest
-from .test_api import TestApi
-#from .test_binding import TestBinding
-#from .test_option_parse import TestOptionParse
+from .test_binding import TestBinding
+from .test_dictionary import TestDictionary
+from .test_mecab import TestMecab
 
 def test_suite():
+    '''Returns suite of tests for natto-py'''
     suite = unittest.TestSuite()
-    suite.addTest(TestApi)
-#    suite.addTest(TestBinding)
-##    suite.addTest(TestOptionParse)
+    suite.addTest(TestBinding)
+    suite.addTest(TestDictionary)
+    suite.addTest(TestMecab)
     return suite
