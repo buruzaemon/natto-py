c7e779991c4c8a09c94bae36989be2de913fb973
Brooke M. Fujita <buruzaemon@users.noreply.github.com>
2014-11-06 03:59:28 +0000

    test_binding.py edited online with Bitbucket; refactoring.

--- tests/test_binding.py
+++ tests/test_binding.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 """Test for natto.binding."""
+import natto.binding as binding
 import unittest
 
-import natto.binding as binding
-
 class TestBinding(unittest.TestCase):
-    """Tests the  functions in the natto.binding module."""
+    """Tests the functions in the natto.binding module."""
 
     def test_ffi_libmecab(self):
         ffi = binding._ffi_libmecab()
