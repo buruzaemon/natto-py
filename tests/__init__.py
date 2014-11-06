276371eac6116b4530d9fe466e76030e576c4c98
Brooke M. Fujita <buruzaemon@users.noreply.github.com>
2014-11-06 04:00:06 +0000

    __init__.py edited online with Bitbucket; refactoring.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,7 @@
-__all__ = ['TestApi']
+import os
+from  natto.mecab import MeCab
 
-from .test_api import TestApi
+if not os.getenv(MeCab.MECAB_PATH):
+    raise EnvironmentError("Please set MECAB_PATH before running the tests")
+if not os.getenv(MeCab.MECAB_CHARSET):
+    raise EnvironmentError("Please set MECAB_CHARSET before running the tests")
