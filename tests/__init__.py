f9de2697e57f9705bd447e098c10a5a9756a4ddb
Brooke Fujita <brooke.fujita@gmail.com>
2014-11-06 08:52:06 +0000

    __init__.py edited online with Bitbucket

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,23 @@
 import os
-from  natto.mecab import MeCab
+import sys
+from natto.mecab import MeCab
+from subprocess import Popen, PIPE
 
+# full path to MeCab library is required for testing...
 if not os.getenv(MeCab.MECAB_PATH):
-    raise EnvironmentError("Please set MECAB_PATH before running the tests")
+    raise EnvironmentError('Please set MECAB_PATH before running the tests')
+# as well as the character encoding used internally by MeCab...
 if not os.getenv(MeCab.MECAB_CHARSET):
-    raise EnvironmentError("Please set MECAB_CHARSET before running the tests")
+    raise EnvironmentError('Please set MECAB_CHARSET before running the tests')
+
+# and the mecab executable is invoked during the tests...
+try:
+    res = Popen(['mecab', '-h'], stdout=PIPE).communicate()
+    line = res[0]
+    exp = 'MeCab'
+    if sys.version >= '3':
+        line = line.decode(os.getenv(MeCab.MECAB_CHARSET))
+    if not line.startswith(exp):
+        raise EnvironmentError('Please check your mecab installation')
+except StandardError as err:
+    raise EnvironmentError(err)
