2da446db0e0d03c339c4999aff6d4ad9c053957b
Brooke Fujita <brooke.fujita@gmail.com>
2014-11-07 06:31:24 +0000

    test_dictionary.py edited online with Bitbucket

--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-"""Tests for natto.dictionary."""
+'''Tests for natto.dictionary.'''
 import re
 import unittest
 import natto.mecab as mecab
 
 class TestDictionary(unittest.TestCase):
-    """Tests the behavior of the natto.dictionary.DictionaryInfo class.
+    '''Tests the behavior of the natto.dictionary.DictionaryInfo class.
 
     Assumes that the mecab-ipadic 2.7.0-20070801 dictionary is installed.
     
@@ -14,16 +14,14 @@ class TestDictionary(unittest.TestCase):
     - UTF-16
     - SHIFT-JIS
     - EUC-JP
-    """
-
-    def setUp(self):
-        self.nm = mecab.MeCab()
+    '''
 
     def test_sysdic(self):
-        sysdic = self.nm.dicts[0]
-
-        cs = sysdic.charset.lower()
-        self.assertIn(cs, ['utf-16', 'utf-8', 'utf8', 'shift-jis', 'euc-jp'])
-        self.assertIsNotNone(re.search('sys.dic$', sysdic.filename))
-        self.assertEqual(sysdic.type, 0)
-        self.assertEqual(sysdic.version, 102)
+        with mecab.MeCab() as nm:
+            sysdic = nm.dicts[0]
+    
+            cs = sysdic.charset.lower()
+            self.assertIn(cs, ['utf-16', 'utf-8', 'utf8', 'shift-jis', 'euc-jp'])
+            self.assertIsNotNone(re.search('sys.dic$', sysdic.filename))
+            self.assertEqual(sysdic.type, 0)
+            self.assertEqual(sysdic.version, 102)
