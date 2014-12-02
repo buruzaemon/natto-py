48610bc1ccc1a9371d3d211b4d65e8cb8c552678
Brooke Fujita <brooke.fujita@gmail.com>
2014-12-02 07:13:05 +0000

    test_suite.py edited online with Bitbucket; re-enabling all tests.

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 '''Test suite for natto-py'''
 import unittest
-#from .test_binding import TestBinding
-#from .test_dictionary import TestDictionary
+from .test_binding import TestBinding
+from .test_dictionary import TestDictionary
 from .test_environment import TestMeCabEnv
 from .test_mecab import TestMecab
 
 def test_suite():
     '''Returns suite of tests for natto-py'''
     suite = unittest.TestSuite()
-#    suite.addTest(TestBinding)
-#    suite.addTest(TestDictionary)
+    suite.addTest(TestBinding)
+    suite.addTest(TestDictionary)
     suite.addTest(TestMeCabEnv)
     suite.addTest(TestMecab)
     return suite
